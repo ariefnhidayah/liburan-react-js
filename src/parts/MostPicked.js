e12c045723d12d4d81ba8c29d27aefe332a628c9
Arief <ariefnhidayah@gmail.com>
2020-05-03 12:42:47 +0700

    update react reveal

--- src/parts/MostPicked.js
+++ src/parts/MostPicked.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import Fade from 'react-reveal/Fade'
 
 import Button from 'elements/Button'
 
 export default function MostPicked(props) {
     return (
+        <Fade bottom>
         <section className="container" ref={props.refMostPicked}>
             <h4 className="mb-3">Most Picked</h4>
             <div className="container-grid">
@@ -11,28 +13,31 @@ export default function MostPicked(props) {
                     props.data.map((item, index) => {
                         return (
                             <div key={`mostpicked-${index}`} className={`item column-4 ${index === 0 ? 'item column-4 row-2' : 'item column-4 row-1'}`}>
-                                <div className="card card-featured">
-                                    <div className="tag">
-                                        ${item.price}
-                                        <span className="font-weight-light"> per {item.unit}</span>
+                                <Fade bottom delay={200 * index}>
+                                    <div className="card card-featured">
+                                        <div className="tag">
+                                            ${item.price}
+                                            <span className="font-weight-light"> per {item.unit}</span>
+                                        </div>
+                                        <figure className="img-wrapper">
+                                            <img src={item.imageUrl} alt={item.name} className="img-cover" />
+                                        </figure>
+                                        <div className="meta-wrapper">
+                                            <Button className="stretched-link d-block text-white" type="link" href={`/properties/${item._id}`}>
+                                                <h5>{item.name}</h5>
+                                            </Button>
+                                            <span>
+                                                {item.city}, {item.country}
+                                            </span>
+                                        </div>
                                     </div>
-                                    <figure className="img-wrapper">
-                                        <img src={item.imageUrl} alt={item.name} className="img-cover" />
-                                    </figure>
-                                    <div className="meta-wrapper">
-                                        <Button className="stretched-link d-block text-white" type="link" href={`/properties/${item._id}`}>
-                                            <h5>{item.name}</h5>
-                                        </Button>
-                                        <span>
-                                            {item.city}, {item.country}
-                                        </span>
-                                    </div>
-                                </div>
+                                </Fade>
                             </div>
                         )
                     })
                 }
             </div>
         </section>
+        </Fade>
     )
 }
